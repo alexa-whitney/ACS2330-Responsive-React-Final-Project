456629d1b1aa952599e3be5212b26ac8d8fb15d2
Alexa Whitney <alexawhitney@Alexas-MacBook-Pro.local>
2023-02-14 12:05:29 -0700

    styled title, about, and footer

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './About.css';
 
+
 function About() {
     return (
         <div className="about-container">
@@ -12,5 +13,6 @@ function About() {
         </div>
     )
 }
+
 
 export default About;
