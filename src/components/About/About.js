93829faf7ef7d88fa68d93b012431e95b2e50c9d
Alexa Whitney <alexawhitney@Alexas-MacBook-Pro.local>
2023-02-14 11:45:24 -0700

    trying to fix git issue

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,11 +3,12 @@ import './About.css';
 
 function About() {
     return (
-        <div>
-            <h1>About Alexa's Book Recommendations</h1>
-            <p>Welcome to The Beguiling Bookshelf! Here, you'll find recommendations for the top ten must-read books in the genres of fantasy, horror, and romance. As an avid reader and lover of these genres, I have a passion for discovering and sharing the best books on the market. Whether you're in the mood for an epic fantasy adventure, a hair-raising horror story, or a heartwarming romance, I have something for you. So, come on in, peruse my top picks, and find your next great read. Happy reading!
+        <div className="about-container">
+            <h1 className="about-header">About The Begiuling Bookshelf</h1>
+            <p className="about-text">Welcome! Here, you'll find recommendations for the top ten must-read books in the genres of fantasy, horror, and romance. As an avid reader and lover of these genres, I have a passion for discovering and sharing the best books on the market. Whether you're in the mood for an epic fantasy adventure, a hair-raising horror story, or a heartwarming romance, I have something for you. So, come on in, peruse my top picks, and find your next great read. Happy reading!
+                <br /><span class="signature">XOXO, Alexa</span>
             </p>
-            <img className='alexa' src='https://media.licdn.com/dms/image/C4E03AQHW39pRx9OLOQ/profile-displayphoto-shrink_800_800/0/1660317254782?e=1681344000&v=beta&t=YPaO8Jf9iNBFYO2lCVBSBY54Mz8Q0gwYHfd5dsqUVps' alt='alexa' />
+            <img className="about-image" src="https://avatars.githubusercontent.com/u/112434021?s=400&u=65cabb3242262812e458cef60d95423fab705f5e&v=4" alt="alexa" />
         </div>
     )
 }
