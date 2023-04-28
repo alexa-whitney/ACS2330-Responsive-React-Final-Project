8130b4ca6c0635e05b8a6afd83c677af3b3e31f1
alexa-whitney <alexawhitney@gmail.com>
2023-04-28 09:26:09 -0600

    Updated About component to be responsive and accessible

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,13 +4,19 @@ import './About.css';
 
 function About() {
     return (
-        <div className="about-container">
-            <h1 className="about-header">About The Begiuling Bookshelf</h1>
-            <p className="about-text">Welcome! Here, you'll find curated recommendations from Alexa Whitney's own library in the genres of fantasy, horror, romance, thriller/myster, historical fiction, and nonfiction. As an avid reader and lover of escaping to new worlds, I have a passion for discovering and sharing the best books on the market. Whether you're in the mood for an epic fantasy adventure, a hair-raising horror story, or a heartwarming romance, I have something for you. So, come on in, peruse my top picks, and find your next great read. Happy reading!
-                <br /><span class="signature">XOXO, Alexa</span>
-            </p>
-            <img className="about-image" src="https://avatars.githubusercontent.com/u/112434021?s=400&u=65cabb3242262812e458cef60d95423fab705f5e&v=4" alt="alexa" />
-        </div>
+        <article className="about-container">
+            <header>
+                <h1 className="about-header">About The Begiuling Bookshelf</h1>
+            </header>
+            <section className="about-content">
+                <p className="about-text">Welcome! Here, you'll find curated recommendations from Alexa Whitney's own library in the genres of fantasy, horror, romance, thriller/myster, historical fiction, and nonfiction. As an avid reader and lover of escaping to new worlds, I have a passion for discovering and sharing the best books on the market. Whether you're in the mood for an epic fantasy adventure, a hair-raising horror story, or a heartwarming romance, I have something for you. So, come on in, peruse my top picks, and find your next great read. Happy reading!</p>
+                <p className="signature">XOXO, Alexa</p>
+            </section>
+            <figure>
+                <img className="about-image" src="https://avatars.githubusercontent.com/u/112434021?s=400&u=65cabb3242262812e458cef60d95423fab705f5e&v=4" alt="Portrait of Alexa" />
+                <figcaption className="visually-hidden">Portrait of Alexa Whitney</figcaption>
+            </figure>
+        </article>
     )
 }
 
